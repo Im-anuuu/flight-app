6ae6fd3e152eeb1c73944d88c94e7eead5a55d86
Im-anuuu <159413902+Im-anuuu@users.noreply.github.com>
2026-06-21 12:36:18 +0530

    update model and app

--- app.py
+++ app.py
@@ -82,14 +82,14 @@ def predict():
     )
 
 
-@app.route('/how-it-works')
-def how_it_works():
-    return render_template('how_it_works.html')
+#@app.route('/how-it-works')
+#def how_it_works():
+#    return render_template('how_it_works.html')
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+#@app.route('/about')
+#def about():
+#    return render_template('about.html')
 
 
 if __name__ == '__main__':
